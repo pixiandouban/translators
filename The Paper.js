59a9b3fc7d9018a83dba5fe525f8519016ad469d
pixiandouban <jackie007atchan@gmail.com>
2021-11-24 12:29:11 +0800

    Update The Paper.js
    add blogPost type

--- The Paper.js
+++ The Paper.js
@@ -9,14 +9,22 @@
 	"inRepository": true,
 	"translatorType": 4,
 	"browserSupport": "gcsibv",
-	"lastUpdated": "2021-11-23 15:12:33"
+	"lastUpdated": "2021-11-24 04:28:07"
 }
 
 function detectWeb(doc, url) {
 	// TODO: adjust the logic here
-	if(url.includes('newsDetail_forward'))
+	
+	var channel = doc.body.querySelector("div.newscontent > div.news_path > a").innerText;
+
+	if(url.includes('newsDetail_forward') && channel != '澎湃号')
 	{
 		return "newspaperArticle";
+	}
+	else if(url.includes('newsDetail_forward') && channel === '澎湃号')
+	{
+		//third-party article
+		return "blogPost";
 	}
 	else{
 		return "multiple";
@@ -55,31 +63,63 @@ function doWeb(doc, url) {
 }
 
 function scrape(doc, url) {
-	var item = new Zotero.Item("newspaperArticle");
+	
+	var type = detectWeb(doc, url);	
+	var item = null;
+	var user = null;
+	if(type === "blogPost")
+	{
+		item = new Zotero.Item("blogPost");		
+		user = doc.body.querySelectorAll("div.newscontent > div.news_path > a");
+		Z.debug("user: " + user[1].innerText);
+		item.blogTitle = user[1].innerText;
+	}
+	else{
+		item = new Zotero.Item("newspaperArticle");		
+		item.publicationTitle = "澎湃新闻";
+	}
+	
 	
 	var tt = ZU.xpathText(doc, '//div[@class="newscontent"]/h1[@class="news_title"]');
-	Z.debug(tt);
+	Z.debug("Title: "+tt);
 	item.title=tt;
 	var authors = doc.body.querySelector("div.news_about > p");
 	authors=authors.innerText.replace(/(澎湃新闻记者 |澎湃首席评论员 )/, ""); //remove prefix label
-	Z.debug(authors);
-	item.creators.push(ZU.cleanAuthor((authors), "author"));
-	Z.debug(item.creators);
+	//Z.debug(authors);
+	if(authors !== null && authors !== "")
+	{
+		Z.debug("author: "+authors);
+		item.creators.push(ZU.cleanAuthor((authors), "author"));		
+		Z.debug("creators: " +item.creators);		
+		
+	}
+	
 	item.language='zh-CN';
 	item.url=url;
 	item.abstractNote = ZU.xpathText(doc, '//meta[@name="Description"]/@content');
-	Z.debug(item.abstractNote);
-	item.publicationTitle = "澎湃新闻";
-	//item.CN = "44-0003";	
+	Z.debug("abstractNote: " +item.abstractNote);
 	
+	item.type = type;
+	/*
+	if(type == "blogPost")
+	{
+		var user = doc.body.querySelectorAll("div.newscontent > div.news_path > a");
+		//Z.debug(user[1].innerText);
+		item.blogTitle = user[1].innerText;
+	}
+	else{
+		item.publicationTitle = "澎湃新闻";
+	}
+	*/
+
 	var pubDate = doc.body.querySelector(".newscontent").querySelectorAll('p');
 	var publicationDate = pubDate[1].innerText.replace(" 来源：澎湃新闻", "");
 	//var publicationDate = ZU.xpathText(doc, "(//div[@class='bd_block'])/span[@id='pubtime_baidu']");
 	//var publicationDate=doc.body.getElementsByClassName('nfzm-content__publish')[0].getAttribute('data-time');
 	if (publicationDate) {
+		Z.debug("datetime: " + publicationDate);		
 		item.date = ZU.strToISO(publicationDate);
 	}
-	Z.debug(publicationDate);
 	
 	//item.accessDate = new Date().toISOString().slice(0, 10);
 	
@@ -136,13 +176,25 @@ var testCases = [
 	},
 	{
 		"type": "web",
+		"url": "https://www.thepaper.cn/newsDetail_forward_15518554",
+		"items": [
+			{
+				"itemType": "blogPost",
+				"language": "zh-CN",
+				"title": "一个雕塑系毕业生的“转型”，从住在精神病院、和病人交朋友开始",
+				"date": "2021-11-24"
+			}
+		]
+	},	
+	{
+		"type": "web",
 		"url": "https://www.thepaper.cn/list_27224",
-		"items": "multiple"		
+		"items": "multiple"
 	},
 	{
 		"type": "web",
 		"url": "https://www.thepaper.cn/searchResult.jsp",
-		"items": "multiple"		
-	}	
+		"items": "multiple"
+	}
 ]
 /** END TEST CASES **/
